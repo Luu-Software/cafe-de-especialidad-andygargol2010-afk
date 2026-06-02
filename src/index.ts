3b24d3777c66919eda50247f0e485325ba718879
andygargol2010-afk <andygargol2010@gmail.com>
2026-06-02 20:19:16 -0300

    .
    .

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,16 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
-console.log(`Total por persona: $${totalPorPersona}`);
+let totalPorPersona: number = 0;
+
+if (esEstudiante.toLowerCase() !== 'no' && esEstudiante.toLowerCase() !== 'si') {
+    console.log('Respuesta no válida.');
+}
+else {
+    if (esEstudiante.toLowerCase() === 'si') {
+        totalTexto = (parseFloat(totalTexto) * 0.9).toString();
+    }
+
+    totalPorPersona = parseFloat(totalTexto) / parseInt(personasTexto);
+    console.log(`Total por persona: $${totalPorPersona}`);
+}
